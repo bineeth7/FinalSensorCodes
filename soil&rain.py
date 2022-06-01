0b4a401970f6f25ea2de1c46574067cc50729c60
Bineeth B C <47277153+bineeth7@users.noreply.github.com>
2022-06-01 12:27:51 +0530

    Update soil&rain.py

--- soil&rain.py
+++ soil&rain.py
@@ -15,12 +15,19 @@ def analogInput(channel):
   return data
 while True:
   output = analogInput(0) # Reading from CH0
+  output1 = analogInput(1) # Reading from CH1
   output = interp(output, [224, 1023], [100, 0])
+  output1 = interp(output1, [270, 1023], [100, 0])
   output=int(output)
+  output1=int(output1)
   print("Rainfall:", output)
+  print("Moisture:", output1)
   output=str(output)
+  output1=str(output1)
   output=output + " %"
+  output1=output1 + " %"
   print(output)
+  print(output1)
   #FBConn.put('https://sensor-readings-142b2.firebaseio.com',"Moisture",output)
   #soil mositure code
 # Importing modules
@@ -42,30 +49,16 @@ while True:
 # spi.open(0,0) 
 
 # Read MCP3008 data
-def analogInput(channel):
+#def analogInput(channel):
 
-  spi.max_speed_hz = 1350000
+#  spi.max_speed_hz = 1350000
 
-  adc = spi.xfer2([1,(8+channel)<<4,0])
+#  adc = spi.xfer2([1,(8+channel)<<4,0])
 
-  data = ((adc[1]&3) << 8) + adc[2]
+#  data = ((adc[1]&3) << 8) + adc[2]
+#  return data
 
-  return data
+# while True:
 
-while True:
-
-  output1 = analogInput(1) # Reading from CH0
-
-  output1 = interp(output1, [270, 1023], [100, 0])
-
-  output1=int(output1)
-
-  print("Moisture:", output1)
-
-  output1=str(output1)
-
-  output1=output1 + " %"
-
-  print(output1)
-
+ 
   #FBConn.put('https://sensor-readings-142b2.firebaseio.com',"Moisture",output)
